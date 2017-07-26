c54118adf134f410b39cb0e13cb5aa0253909e6c
Scott Schaefer <scottschaef@gmail.com>
2017-07-26 19:46:14 -0400

    Returned index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs');
+
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var content;
+  // get path
+  // archive.paths.siteAssets
+  // using read file, read it and send it back
+  fs.readFile(archive.paths.siteAssets+'/index.html', 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    content = data;
+    console.log(JSON.stringify(content));
+
+    res.end(content);
+  });
+
+
+  // res.end(archive.paths.list);
 };
+
+
